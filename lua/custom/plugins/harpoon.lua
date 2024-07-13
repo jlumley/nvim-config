4b797fe726d13b5ebfc565ea8558725d4dc3ee5c
Jeremy Lumley <jlumley@tucows.com>
2024-07-13 12:22:08 -0400

    better documentation for leader key commands

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -10,27 +10,27 @@ return {
       -- adding new entry
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():add()
-      end, { desc = 'Add new harpoon entry' })
+      end, { desc = '[A]dd new harpoon entry' })
       -- open quick menu
       vim.keymap.set('n', '<leader>m', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
+      end, { desc = 'Open Harpoon quick [m]enu' })
       -- select index 1
       vim.keymap.set('n', '<leader>j', function()
         harpoon:list():select(1)
-      end)
+      end, { desc = 'Select Harpoon index 1' })
       -- select index 2
       vim.keymap.set('n', '<leader>k', function()
         harpoon:list():select(2)
-      end)
+      end, { desc = 'Select Harpoon index 2' })
       -- select index 3
       vim.keymap.set('n', '<leader>l', function()
         harpoon:list():select(3)
-      end)
+      end, { desc = 'Select Harpoon index 3' })
       -- select index 4
       vim.keymap.set('n', "<leader>'", function()
         harpoon:list():select(4)
-      end)
+      end, { desc = 'Select Harpoon index 4' })
     end,
   },
 }
