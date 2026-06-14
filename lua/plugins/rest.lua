3be44b679d3563c4be7bdcd3517d8f97ddd1672e
Jeremy Lumley <jeremy.lumley96@gmail.com>
2026-06-14 14:20:16 -0400

    remove rest.nvim

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -1,29 +1,29 @@
-return {
-  {
-    'rest-nvim/rest.nvim',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      opts = function(_, opts)
-        opts.ensure_installed = opts.ensure_installed or {}
-        table.insert(opts.ensure_installed, 'http')
-      end,
-    },
-    ft = { 'http' },
-    config = function()
-      vim.keymap.set('n', '<leader>sr', function()
-        vim.cmd 'Rest run'
-      end, { desc = 'Rest [S]end [R]equest' })
-
-      vim.g.rest_nvim = {
-        request = {
-          -- skip_ssl_verification = true,
-        },
-        -- Highlight request on run
-        highlight = {
-          enabled = true,
-          timeout = 150,
-        },
-      }
-    end,
-  },
-}
+-- return {
+--   {
+--     'rest-nvim/rest.nvim',
+--     dependencies = {
+--       'nvim-treesitter/nvim-treesitter',
+--       opts = function(_, opts)
+--         opts.ensure_installed = opts.ensure_installed or {}
+--         table.insert(opts.ensure_installed, 'http')
+--       end,
+--     },
+--     ft = { 'http' },
+--     config = function()
+--       vim.keymap.set('n', '<leader>sr', function()
+--         vim.cmd 'Rest run'
+--       end, { desc = 'Rest [S]end [R]equest' })
+--
+--       vim.g.rest_nvim = {
+--         request = {
+--           -- skip_ssl_verification = true,
+--         },
+--         -- Highlight request on run
+--         highlight = {
+--           enabled = true,
+--           timeout = 150,
+--         },
+--       }
+--     end,
+--   },
+-- }
