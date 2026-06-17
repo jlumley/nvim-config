766d12591ae640d4a2c6834583d1654d174cdf79
Jeremy Lumley <jlumley@tucows.com>
2026-06-17 16:16:23 -0400

    update conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,42 +1,42 @@
 -- filepath: lua/custom/plugins/conform.lua
 return { -- Autoformat
-  'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
-  cmd = { 'ConformInfo' },
-  keys = {
-    {
-      '<leader>f',
-      function()
-        require('conform').format { async = true, lsp_format = 'fallback' }
-      end,
-      mode = '',
-      desc = '[F]ormat buffer',
-    },
-  },
-  opts = {
-    notify_on_error = false,
-
-    -- Configure format-on-save behavior
-    format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true, typescript = true }
-      local lsp_format_opt = disable_filetypes[vim.bo[bufnr].filetype] and 'never' or 'fallback'
-
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
-    end,
-
-    -- Formatter configuration by file type
-    formatters_by_ft = {
-      -- Scripting languages
-      lua = { 'stylua' },
-      python = { 'black' },
-      bash = { 'shfmt' },
-
-      -- Web development (runs in order, use multiple for fallback)
-      typescript = { 'prettierd', 'prettier' },
-      -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
-    },
-  },
+  -- 'stevearc/conform.nvim',
+  -- event = { 'BufWritePre' },
+  -- cmd = { 'ConformInfo' },
+  -- keys = {
+  --   {
+  --     '<leader>f',
+  --     function()
+  --       require('conform').format { async = true, lsp_format = 'fallback' }
+  --     end,
+  --     mode = '',
+  --     desc = '[F]ormat buffer',
+  --   },
+  -- },
+  -- opts = {
+  --   notify_on_error = false,
+  --
+  --   -- Configure format-on-save behavior
+  --   format_on_save = function(bufnr)
+  --     local disable_filetypes = { c = true, cpp = true, typescript = true }
+  --     local lsp_format_opt = disable_filetypes[vim.bo[bufnr].filetype] and 'never' or 'fallback'
+  --
+  --     return {
+  --       timeout_ms = 500,
+  --       lsp_format = lsp_format_opt,
+  --     }
+  --   end,
+  --
+  --   -- Formatter configuration by file type
+  --   formatters_by_ft = {
+  --     -- Scripting languages
+  --     lua = { 'stylua' },
+  --     python = { 'black' },
+  --     bash = { 'shfmt' },
+  --
+  --     -- Web development (runs in order, use multiple for fallback)
+  --     typescript = { 'prettierd', 'prettier' },
+  --     -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
+  --   },
+  -- },
 }
